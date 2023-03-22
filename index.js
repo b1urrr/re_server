d88acb902bae0d4261693af36b8bdbeba1877ba8
b1urrr <kristiangogow@gmail.com>
2023-03-22 09:31:32 +0200

    changed port to 80

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 })
 
 const CONNECTION_URL = process.env.MONGO_URL;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
