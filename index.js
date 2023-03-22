ec56095e6ac848241b0eec83e6235a71b6120671
b1urrr <kristiangogow@gmail.com>
2023-03-22 10:17:53 +0200

    vercel config

--- index.js
+++ index.js
@@ -25,6 +25,3 @@ const PORT = process.env.PORT || 80;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error));
-
-
-export default function app()
